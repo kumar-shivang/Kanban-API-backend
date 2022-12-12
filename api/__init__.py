39aabb00bbcf0abc963c6d9b21d2049b511f230e
Shivang Kumar <shivangkumar1857@gmail.com>
2022-12-12 23:23:18 +0530

    > This commit is a huge bug fix > There was a bug in database module. > Had to put all models in the __init__ file > had to do a few changes ina app.py and api > now database is working

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,18 @@
 #This file initialize the flask api. It also contains the routes for the api.
-from flask_api import FlaskAPI # import the Flask API from the flask_api module
-from database import db # import the database object from the database folder
-from database.card_model import Card # import the Card class from the card.model file in the database/card folder
-api = FlaskAPI() #Initialize the flask api
+from flask import Blueprint, jsonify # import the Blueprint class and jsonify method from the flask module
+from database import db, User, List, Card # import the database object from the database folder
+# from database.card_model import Card # import the Card class from the card.model file in the database/card folder
+from datetime import date, timedelta # import the date and timedelta classes from the datetime module
+API = Blueprint('API', __name__) # Initialize the API blueprint
 
-@api.route('/cardID/<int:cardID>', methods=['GET']) #This route returns the card with the given cardID
+@API.route('/cardID/<int:cardID>', methods=['GET']) #This route returns the card with the given cardID
 def getCard(cardID):
-    return db.session.query(Card).filter(Card.cardID == cardID).first()
+    c = db.session.query(Card).filter(Card.cardID == cardID).first()
+    return jsonify({"cardID": c.cardID, "title": c.title, "content": c.content, "deadline": c.deadline.strftime("%d-%m-%Y")}),200
 
-    
+@API.route('/create/card/<int:listID>/<title>/<int:deadline>', methods=['POST']) #This route creates a card with the given title and deadline in the list with the given listID
+def createCard(listID, title, deadline):
+    c = Card(title, " ", date.today() + timedelta(days=deadline), listID)
+    db.session.add(c)
+    db.session.commit()
+    return jsonify({c.cardID : "Successfully created"}), 200
--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 # Description: This is the main file of the application. It is used to run the application.
 from flask import Flask # Import the Flask class from the flask module
-from database import db # Import the database object from the database folder
-from api import api # Import the api object from the api folder
+from database import db,User,Card # Import the database object from the database folder
+from api import API # Import the api object from the api folder
+
 
 
 app = Flask(__name__) #Initialize the flask app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #This is the path to the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite' #This is the path to the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #This is to disable the modification tracker
 app.config['SECRET_KEY'] = 'screat key' #This is the secret key for the session
-db.init_app(app) #Initialize the database
-api.init_app(app) #Initialize the api
+with app.app_context():
+    db.init_app(app) #Initialize the database
+    db.create_all() #Create the database
+    app.register_blueprint(API) #Register the api blueprint
 
 if __name__ == '__main__':
     app.run()
