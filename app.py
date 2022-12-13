715afa9f0929fe9e1881c339cca467e5910032ad
Shivang Kumar <shivangkumar1857@gmail.com>
2022-12-13 17:04:02 +0530

    added CORS module and intialized CORS in app.py file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Description: This is the main file of the application. It is used to run the application.
 from flask import Flask # Import the Flask class from the flask module
-from database import db,User,Card # Import the database object from the database folder
+from database import db # Import the database object from the database folder
 from api import API # Import the api object from the api folder
-
+from flask_cors import CORS # Import the CORS class from the flask_cors module
 
 
 app = Flask(__name__) #Initialize the flask app
@@ -13,6 +13,6 @@ with app.app_context():
     db.init_app(app) #Initialize the database
     db.create_all() #Create the database
     app.register_blueprint(API) #Register the api blueprint
-
+    CORS(app) #Initialize the CORS for the API
 if __name__ == '__main__':
     app.run()
