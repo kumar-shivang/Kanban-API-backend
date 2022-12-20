b3826749534ff1074698b403342b461a292cbff2
Shivang Kumar <shivangkumar1857@gmail.com>
2022-12-20 21:36:27 +0530

    edited app.py and added config module

--- app.py
+++ app.py
@@ -1,23 +1,43 @@
 # Description: This is the main file of the application. It is used to run the application.
-import os
 from flask import Flask  # Import the Flask class from the flask module
-from database import db  # Import the database object from the database folder
-from api import API, jwt  # Import the API object from the api folder
+from api import jwt  # Import the API object from the api folder
+from datetime import timedelta
 from api.userAPI import *  # Import all the routes from the userAPI
 from api.listAPI import *  # Import all the routes from the listAPI
 from api.cardAPI import *  # Import all the routes from the cardAPI
 from api.authAPI import *  # Import all the routes from the authAPI
+from flask_jwt_extended import get_jwt
+from config import DevelopmentConfig  # Import the DevelopmentConfig class from the config folder
 
 
-app = Flask(__name__)  # Initialize the flask app
-current_dir = os.path.dirname(os.path.realpath(__file__))  # Get the current directory
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(current_dir, 'database.db')  # Set the database URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # This is to disable the modification tracker
-app.config['SECRET_KEY'] = 'secret key'  # This is the secret key for the session
-app.config['JWT_SECRET'] = 'jwt secret key'  # This is the secret key for the JWT
-db.init_app(app)  # Initialize the database
-jwt.init_app(app)
-app.register_blueprint(API)  # Register the API blueprint  # Initialize the JWTManager
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
+    jwt.init_app(app)
+    app.register_blueprint(API)
+    app.app_context().push()
+    with app.app_context():
+        db.create_all()
+    return app
+
+
+app = create_app()
+
+
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()['exp']
+        now = datetime.utcnow()
+        target_timestamp = datetime.timestamp(now + timedelta(seconds=app.config['JWT_REFRESH_EXPIRATION_DELTA']))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)  # Run the application
