154576e933ec506473ab0512380c1131595c779b
Shivang Kumar <shivangkumar1857@gmail.com>
2023-01-10 14:15:07 +0530

    imported celery worker and created a test route

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 # Description: This is the main file of the application. It is used to run the application.
 from flask import Flask  # Import the Flask class from the flask module
+from flask_cors import CORS  # Import the CORS class from the flask_cors module
+import worker
 from api import jwt  # Import the API object from the api folder
-from datetime import timedelta
-from api.userAPI import *  # Import all the routes from the userAPI
-from api.listAPI import *  # Import all the routes from the listAPI
 from api.cardAPI import *  # Import all the routes from the cardAPI
-from api.authAPI import *  # Import all the routes from the authAPI
-from flask_jwt_extended import get_jwt
 from config import DevelopmentConfig  # Import the DevelopmentConfig class from the config folder
-
+from worker.task import say_hello
 
 def create_app():
     app = Flask(__name__)
@@ -19,24 +16,19 @@ def create_app():
     app.app_context().push()
     with app.app_context():
         db.create_all()
-    return app
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    celery = worker.celery
+    celery.Task = worker.ContextTask
+    return app, celery
 
 
-app = create_app()
+app, celery = create_app()
 
-
-@app.after_request
-def refresh_expiring_jwts(response):
-    try:
-        exp_timestamp = get_jwt()['exp']
-        now = datetime.utcnow()
-        target_timestamp = datetime.timestamp(now + timedelta(seconds=app.config['JWT_REFRESH_EXPIRATION_DELTA']))
-        if target_timestamp > exp_timestamp:
-            access_token = create_access_token(identity=get_jwt_identity())
-            set_access_cookies(response, access_token)
-        return response
-    except (RuntimeError, KeyError):
-        return response
+@app.route("/hello/<name>")
+def hello(name):
+    job = say_hello.delay(name)
+    result = job.wait()
+    return result, 200
 
 
 if __name__ == '__main__':
