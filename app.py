096a8803dc9a99e026c710d11b4c1cee771f7fa4
Shivang Kumar <shivangkumar1857@gmail.com>
2023-01-14 15:46:10 +0530

    added mail, cache and celery

--- app.py
+++ app.py
@@ -1,34 +1,37 @@
 # Description: This is the main file of the application. It is used to run the application.
-from flask import Flask  # Import the Flask class from the flask module
+from flask import Flask # Import the Flask class from the flask module
 from flask_cors import CORS  # Import the CORS class from the flask_cors module
-import worker
+import jobs
+from cache import cache
 from api import jwt  # Import the API object from the api folder
 from api.cardAPI import *  # Import all the routes from the cardAPI
+from api.userAPI import *
+from api.authAPI import *
+from api.listAPI import *
+from api.exportAPI import *
 from config import DevelopmentConfig  # Import the DevelopmentConfig class from the config folder
-from worker.task import say_hello
+from mail import mail
+from api import API  # Import the API object from the api folder
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
     db.init_app(app)
     jwt.init_app(app)
+    cache.init_app(app)
+    mail.init_app(app)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     app.register_blueprint(API)
     app.app_context().push()
     with app.app_context():
         db.create_all()
-    cors = CORS(app, resources={r"/*": {"origins": "*"}})
-    celery = worker.celery
-    celery.Task = worker.ContextTask
+    celery = jobs.celery
+    celery.Task = jobs.ContextTask
     return app, celery
 
 
 app, celery = create_app()
-
-@app.route("/hello/<name>")
-def hello(name):
-    job = say_hello.delay(name)
-    result = job.wait()
-    return result, 200
 
 
 if __name__ == '__main__':
