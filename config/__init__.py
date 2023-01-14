47c51d065c39c5d9a1336466d13f612b2e63ca69
Shivang Kumar <shivangkumar1857@gmail.com>
2023-01-14 15:42:47 +0530

    updated config file

--- config/__init__.py
+++ config/__init__.py
@@ -3,13 +3,13 @@ from datetime import timedelta
 from os import path
 basedir = path.abspath(path.dirname(__file__))
 class Config(object):
-    SQLALCHEMY_DB_DIR = None
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
     JWT_REFRESH_TOKEN_EXPIRES = 30
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
-    JWT_TOKEN_LOCATION = ['cookies','json']
+    JWT_TOKEN_LOCATION = ['cookies','json','headers']
+    JWT_ACCESS_COOKIE_NAME = 'access_token'
     JWT_COOKIE_CSRF_PROTECT = False
     JWT_COOKIE_SECURE = False
 
@@ -21,3 +21,9 @@ class DevelopmentConfig(Config):
     TEST = True
     SECRET_KEY = 'test_secret_key'
     JWT_SECRET = 'test_jwt-secret'
+    MAIL_SERVER = "0.0.0.0"
+    MAIL_PORT = 1025
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = False
+
+
